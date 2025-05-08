b82016f43d3d2a354ac30cab018799b6f87b3e45
layan <layanazez@gmail.com>
2025-05-08 09:49:03 +0000

    update file chat

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -2,13 +2,13 @@
 import sys
 
 def factorial(n):
-result = 1
-while n > 1:
-result *= n
-n -= 1
+    result = 1  # This line must be **indented**
+    while n > 1:  # Ensure this block is also properly indented
+        result *= n
+        n -= 1  
 
-return result
+    return result  # Indentation must be consistent
 
 if __name__ == "__main__":
-f = factorial(int(sys.argv[1]))
-print(f)
+    f = factorial(int(sys.argv[1]))
+    print(f)
